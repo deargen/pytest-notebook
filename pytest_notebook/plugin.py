a637739a6036f3142e242704e18936e7e03a87d5
Kiyoon Kim <kiyoon@users.noreply.github.com>
2025-01-31 09:43:35 +0900

    fix: pytest9 deprecation

--- pytest_notebook/plugin.py
+++ pytest_notebook/plugin.py
@@ -1,4 +1,5 @@
-"""pytest plugin configuration.
+"""
+pytest plugin configuration.
 
 For more information on writing pytest plugins see:
 
@@ -10,7 +11,9 @@ For more information on writing pytest plugins see:
 - https://docs.pytest.org/en/latest/_modules/_pytest/hookspec.html
 
 """
+
 import os
+from pathlib import Path
 import shlex
 
 import pytest
@@ -125,7 +128,8 @@ def pytest_addoption(parser):
 
 
 def str2bool(string):
-    """Convert a string representation of truth to True or False.
+    """
+    Convert a string representation of truth to True or False.
 
     True values are 'y', 'yes', 't', 'true', 'on', and '1'; false values
     are 'n', 'no', 'f', 'false', 'off', and '0'.  Raises ValueError if
@@ -165,7 +169,8 @@ def strip_quotes(string):
 
 
 def validate_diff_replace(pytestconfig):
-    r"""Extract the ``nb_diff_replace`` option from the ini file.
+    r"""
+    Extract the ``nb_diff_replace`` option from the ini file.
 
     This should be of the format::
 
@@ -190,7 +195,8 @@ def validate_diff_replace(pytestconfig):
 
 
 def gather_config_options(pytestconfig):
-    """Gather all options, from command-line and ini file.
+    """
+    Gather all options, from command-line and ini file.
 
     Note: command-line set options are prioritised over ini file ones.
     """
@@ -243,7 +249,6 @@ def gather_config_options(pytestconfig):
 
 def pytest_report_header(config):
     """Add header information for pytest execution."""
-
     kwargs, other_args = gather_config_options(config)
     header = []
     if kwargs.get("exec_notebook", True) and kwargs.get("exec_cwd", None):
@@ -258,7 +263,6 @@ def pytest_report_header(config):
 @pytest.fixture(scope="function")
 def nb_regression(pytestconfig):
     """Fixture to execute a Jupyter Notebook, and test its output is as expected."""
-
     kwargs, other_args = gather_config_options(pytestconfig)
     return NBRegressionFixture(**kwargs)
 
@@ -270,13 +274,14 @@ def pytest_collect_file(path, parent):
         path.fnmatch(pat) for pat in other_args.get("nb_file_fnmatch", ["*.ipynb"])
     ):
         try:
-            return JupyterNbCollector.from_parent(parent, fspath=path)
+            return JupyterNbCollector.from_parent(parent, path=Path(path))
         except AttributeError:
             return JupyterNbCollector(path, parent)
 
 
 class JupyterNbCollector(pytest.File):
-    """This class represents a pytest collector object for Jupyter Notebook files.
+    """
+    This class represents a pytest collector object for Jupyter Notebook files.
 
     A collector is associated with a .ipynb file and collects it for testing.
     """
@@ -310,7 +315,8 @@ class JupyterNbTest(pytest.Item):
         fixture.check(self.fspath)
 
     def repr_failure(self, exc_info):
-        """Handle exception raised by ``self.runtest()``.
+        """
+        Handle exception raised by ``self.runtest()``.
 
         :param exc_info: see
             https://docs.pytest.org/en/latest/reference.html#_pytest._code.ExceptionInfo
